735549f516a23e4abd70aca37988e7bb1ffe2d8a
dav3794 <104735074+dav3794@users.noreply.github.com>
2024-11-12 23:46:55 +0100

    Upload model notebooks

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,77 @@
+import os
+import numpy as np
+import tifffile
+import anndata
+import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
 
+from torchvision.transforms import RandomResizedCrop
+from torch.utils.data import Dataset
+from glob import glob
 from typing import Tuple, List
+
+
+class DatasetFromTIFF(Dataset):
+    def __init__(self, img_path: str, transform=None):
+        self.imgs = glob(os.path.join(img_path, '*.tiff'))
+        self.transform = transform
+    
+    @staticmethod
+    def preprocess(img):
+        return np.arcsinh(img / 5.0)
+    
+    @staticmethod
+    def norm_minmax(img):
+        min_val = np.min(img, axis=(1,2), keepdims=True)
+        max_val = np.max(img, axis=(1,2), keepdims=True)
+        scaled_img = (img - min_val) / (max_val - min_val)
+        return torch.tensor(scaled_img)
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        img = tifffile.imread(self.imgs[idx])
+        img = self.preprocess(img)
+        img = self.norm_minmax(img)
+        if self.transform:
+            img = self.transform(img)
+        channel_ids = torch.arange(img.shape[0], dtype=torch.long)
+        return img, channel_ids
+
+
+def plot_markers(image: np.ndarray, marker_names: dict[int:str], cmap: str = 'CMRmap', save_path: str = None):
+    """Plot all the markers in the image 
+
+    Args:
+        image (np.ndarray): Image to plot
+        marker_names (dict[int:str]): Mapper from marker index to marker name
+        cmap (str, optional): Color map to use. Defaults to 'CMRmap'.
+        save_path (str, optional): Path to save the plot. Defaults to None.
+    """
+    fig, axs = plt.subplots(8, 5, figsize=(12, 20))
+    for i, ax in enumerate(axs.flat):
+        ax.imshow(image[i], cmap=cmap)
+        ax.axis('off')
+        ax.set_title(marker_names[i])
+    plt.tight_layout()
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+
+def get_marker_names_map(adata: anndata.AnnData) -> dict[int:str]:
+    """Get the marker names from the anndata object
+
+    Args:
+        adata (anndata.AnnData): Anndata object
+
+    Returns:
+        dict[int:str]: Mapper from marker index to marker name
+    """
+    df = adata.var.copy()
+    df.index = df.index.astype(int)
+    return df['marker'].to_dict()
 
 # code adapted from https://github.com/facebookresearch/segment-anything/blob/main/segment_anything/modeling/image_encoder.py
 # and https://github.com/facebookresearch/detectron2/blob/main/detectron2/modeling/backbone/vit.py
@@ -55,7 +125,7 @@ def window_unpartition(
     """
     shift_y, shift_x = shift
     Hp, Wp = pad_hw
-    B, C, H, W, E = orig_shape
+    B, H, W, C = orig_shape
 
     x = windows.view(B, Hp // window_size, Wp // window_size, window_size, window_size, -1)
     x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, Hp, Wp, -1)
